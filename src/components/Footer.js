f791d6da450ae6a783f96c15010488ccc7128e9d
Pneuro <57775304+Pneuro@users.noreply.github.com>
2021-01-15 08:09:55 -0500

    initial commit - building MVP

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <div>
       <footer>
         <h2>Dry John Toilet Bags</h2>
-        <p>Poop with confidence.</p>
+        <p>Eco friendly</p>
       </footer>
     </div>
   );
