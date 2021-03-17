e2ec135a1fb9916f45a117f7beda5e43d326fb26
Pneuro <57775304+Pneuro@users.noreply.github.com>
2021-03-17 19:30:01 -0400

    testing deploys

--- src/prismic-configuration.js
+++ src/prismic-configuration.js
@@ -2,7 +2,7 @@ import Prismic from "@prismicio/client";
 
 export const apiEndpoint = "https://dry-john.cdn.prismic.io/api/v2";
 export const accessToken =
-  "MC5ZRTlWNmhJQUFDWUFyQWM5.Te-_ve-_vTPvv73vv71OB--_ve-_ve-_vVPvv73vv71F77-977-9RX4777-977-977-977-977-9NO-_vUYaBgPvv70";
+  "1MC5ZRTlWNmhJQUFDWUFyQWM5.Te-_ve-_vTPvv73vv71OB--_ve-_ve-_vVPvv73vv71F77-977-9RX4777-977-977-977-977-9NO-_vUYaBgPvv70";
 
 // Client method to query documents from the Prismic repo
 export const Client = (req = null) =>
