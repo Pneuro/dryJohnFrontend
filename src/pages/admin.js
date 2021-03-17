fd8af60fcf8762196b0826e7205a4dcc92076505
Pneuro <57775304+Pneuro@users.noreply.github.com>
2021-03-17 10:52:12 -0400

    updates

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -6,37 +6,6 @@ import { useState } from "react";
 import Footer from "../components/Footer";
 
 const backendPage = ({ user, logout }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const emailUpdate = (e) => setEmail(e.target.value);
-  const passwordUpdate = (e) => setPassword(e.target.value);
-
-  const data = [email, password];
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(data);
-    fetch("https://dry-john-backend.herokuapp.com/login", headers).then((res) =>
-      res
-        .json()
-        .then((res) => {
-          console.log(res.data);
-          if (res === 200) {
-            console.log(res);
-            setLogin(true);
-          } else {
-            console.log(res);
-          }
-        })
-        .catch((err) => console.error(err))
-    );
-  };
-  const headers = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  };
-
   const styles = {
     wrapper: {
       marginTop: "100px",
